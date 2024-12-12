70bd7689e934924bd1cb3d608100cc81b606e837
Mcp1006 <maxencecp1006@gmail.com>
2024-12-12 17:40:09 +0100

    CNY70

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 
 rgb_lcd lcd;
 ESP32Encoder encoder;
-int BP0=0, BP1=2, BP2=12, POT=33, PWM=27, DIRECTION=26, a;
-int ValBP0=0, ValBP1=0, ValBP2=0, ValPOT=0;
+int BP0=0, BP1=2, BP2=12, POT=33, PWM=27, DIRECTION=26, a, CNY70=36;
+int ValBP0=0, ValBP1=0, ValBP2=0, ValPOT=0,ValCNY70=0;
 void setup() {
   // Initialise la liaison avec le terminal
   Serial.begin(115200);
@@ -20,6 +20,7 @@ void setup() {
   pinMode(POT,INPUT);
   pinMode(PWM,OUTPUT);
   pinMode(DIRECTION,OUTPUT);
+  pinMode(CNY70,INPUT);
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(8, 2, LCD_5x8DOTS, Wire1);
@@ -27,8 +28,14 @@ void setup() {
   // Initialise PWM
   ledcSetup(0, 500, 10);
   ledcAttachPin(PWM, 0);
-  encoder.attachFullQuad ( DT, CLK );
+  encoder.attachHalfQuad ( DT, CLK );
   encoder.setCount ( 0 );
+  while(ValCNY70<4000)
+  {
+    ValCNY70=analogRead(CNY70);
+    ledcWrite(0, 250);
+  }
+    encoder.setCount ( 0 );
 }
 
 void loop() {
@@ -37,16 +44,18 @@ void loop() {
 ValBP0=digitalRead(BP0);
 ValBP1=digitalRead(BP1);
 ValBP2=digitalRead(BP2);
+ValCNY70=analogRead(CNY70);
+
 
 lcd.setCursor(0,0);
-lcd.printf("BP0:%dBP1:%dBP2:%d",ValBP0,ValBP1,ValBP2);
+lcd.printf("VAL:%4d",ValCNY70);
 //Serial.printf("BP0:%dBP1:%dBP2:%d\n",ValBP0,ValBP1,ValBP2);
 
 
 //lecture et affichage du potetiometre
 ValPOT=analogRead(POT);
-lcd.setCursor(0,1);
-lcd.printf("%4d",ValPOT);
+//lcd.setCursor(0,1);
+//lcd.printf("%4d",ValPOT);
 //Serial.printf("%4d\n",ValPOT);
 
 
@@ -62,6 +71,7 @@ if (ValBP1 == 1)
 ledcWrite(0, ValPOT/4);
   long newPosition = encoder.getCount();
   Serial.println(newPosition);
-
+  lcd.setCursor(0,1);
+  lcd.printf("nbTour:%3d",newPosition/420);
 }
 
