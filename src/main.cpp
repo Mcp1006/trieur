9c8455b24197b82fffba6416db23044b7a671213
Maxcp1006 <maxencecp1006@gmail.com>
2024-12-13 17:40:26 +0100

    SC(en cours)

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,39 @@
 #include <Arduino.h>
 #include "rgb_lcd.h"
 #include <ESP32Encoder.h>
-
+#include "Adafruit_TCS34725.h"
+#include <SPI.h>
 
 #define CLK 23 // A ENCODER 
 #define DT 19 // B ENCODER 
-
-
 rgb_lcd lcd;
 ESP32Encoder encoder;
-int BP0=0, BP1=2, BP2=12, POT=33, PWM=27, DIRECTION=26, a, CNY70=36;
+int BP0=0, BP1=2, BP2=12, POT=33, PWM=27, DIRECTION=26, a, CNY70=36, ETAT = 0, lux, i=0;
 int ValBP0=0, ValBP1=0, ValBP2=0, ValPOT=0,ValCNY70=0;
+long newPosition;
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void position (long pos)
+{
+  lcd.setCursor(0,1);
+  lcd.printf("nbTour:%3d",pos);
+  if (newPosition < (pos*51))
+  {
+    digitalWrite(DIRECTION, HIGH);
+    ledcWrite(0, 250);
+  }
+ if (newPosition > (pos*51))
+  {
+    digitalWrite(DIRECTION, LOW);
+    ledcWrite(0, 250);
+  }
+  if (newPosition == (pos*51))
+  {
+    ledcWrite(0, 0);
+  }
+}
+
 void setup() {
   // Initialise la liaison avec le terminal
   Serial.begin(115200);
@@ -30,12 +53,23 @@ void setup() {
   ledcAttachPin(PWM, 0);
   encoder.attachHalfQuad ( DT, CLK );
   encoder.setCount ( 0 );
+  
   while(ValCNY70<4000)
   {
     ValCNY70=analogRead(CNY70);
-    ledcWrite(0, 250);
+    ledcWrite(0, 230);
   }
     encoder.setCount ( 0 );
+    ledcWrite(0, 0);
+    
+    
+  if (tcs.begin()) {
+    //Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+
 }
 
 void loop() {
@@ -48,7 +82,7 @@ ValCNY70=analogRead(CNY70);
 
 
 lcd.setCursor(0,0);
-lcd.printf("VAL:%4d",ValCNY70);
+//lcd.printf("VAL:%4d",ValCNY70);
 //Serial.printf("BP0:%dBP1:%dBP2:%d\n",ValBP0,ValBP1,ValBP2);
 
 
@@ -60,18 +94,61 @@ ValPOT=analogRead(POT);
 
 
 //commande pwm
-if (ValBP1 == 0)
-{
-  digitalWrite(DIRECTION, HIGH);
-}
-if (ValBP1 == 1)
-{
-  digitalWrite(DIRECTION, LOW);
-}
-ledcWrite(0, ValPOT/4);
-  long newPosition = encoder.getCount();
+
+//ledcWrite(0, ValPOT/4);
+  if (ValCNY70 > 4094) 
+  {
+    encoder.setCount(0);
+  }
+  switch (ETAT)
+  {
+    case 0:
+    {
+      if (ValBP0 == 0)
+      {
+        position(3);
+        ETAT = 1;
+      }
+      if (ValBP1 == 0)
+      {
+        ETAT = 1;
+        position(4);
+      }
+    }
+
+    case 1:
+    {
+      if(newPosition == 208)   //position 4
+      {
+        for (i=0;i<8;i++)
+      }
+      if(newPosition == 156)   //position 3
+      {
+
+      }
+    }
+  }
+  
+  newPosition = encoder.getCount();
   Serial.println(newPosition);
-  lcd.setCursor(0,1);
-  lcd.printf("nbTour:%3d",newPosition/420);
+float red, green, blue;
+  
+  tcs.setInterrupt(false);  // turn on LED
+
+  delay(60);  // takes 50ms to read
+
+  tcs.getRGB(&red, &green, &blue);
+  
+  tcs.setInterrupt(true);  // turn off LED
+
+  Serial.print("R:\t"); Serial.print(int(red)); 
+  Serial.print("\tG:\t"); Serial.print(int(green)); 
+  Serial.print("\tB:\t"); Serial.print(int(blue));
+
+//  Serial.print("\t");
+//  Serial.print((int)red, HEX); Serial.print((int)green, HEX); Serial.print((int)blue, HEX);
+  Serial.print("\n");
+lcd.setRGB(red, green, blue);
+
 }
 
